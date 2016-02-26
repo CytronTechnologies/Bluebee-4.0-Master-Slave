d9a41fc9d8d06c27a6adc4e9db89243ed742f629
Cytron Trainee <support@cytron.com.my>
2016-02-26 10:59:42 +0800

    Added files via upload

---
+++ Master/Master.ino
@@ -0,0 +1,53 @@
+
+const char ledPin = 13;   //red
+#include <Servo.h>
+
+Servo myservo;
+
+byte packet[4];
+byte rcvState=0; 
+
+
+void setup()
+{
+ Serial.begin(115200); //initialize serial communication with baudrate = 9600bps (UART is used for this case)
+ Serial.flush();  //flush out the contents in the receive buffer
+ pinMode(ledPin,OUTPUT); //set the led2 (or digital pin 13) as output
+ pinMode(11,OUTPUT);
+ myservo.attach(9,800 ,2200);
+  rcvState=0;
+}
+
+void loop()
+{
+
+
+    while(Serial.available()<4); //wait for 4 bytes
+
+    for(int i=0;i<4; i++)  //read 4 bytes
+    {
+      packet[i]=Serial.read(); 
+    }
+    
+
+    if(packet[0]==0xFF && packet[1]==0xFF) //header verified correct
+    {
+      if(packet[2]=='S')  //servo packet
+      {
+        myservo.write(packet[3]);        
+      }
+
+      else if(packet[2]=='L')
+      {
+
+        digitalWrite(ledPin, packet[3]);
+      } 
+      else if(packet[2]=='B')
+      {
+        analogWrite(11,packet[3]);
+      }
+    
+    }
+
+}      
+
---
+++ Slave/Slave.ino
@@ -0,0 +1,63 @@
+int potpin = A0;
+int potpin1 = A2;
+byte LEDState=0;
+
+unsigned int val;
+unsigned int val1;
+
+
+void setup()
+{
+ Serial.begin(115200);
+}
+
+void loop()
+{
+  val = analogRead(potpin);
+  val = map(val, 0, 1023, 0, 225);
+
+  val1 = analogRead(potpin1);
+  val1 = map(val1, 0, 1023, 0, 225);
+
+
+  writeServo(val);
+  writeLED(val1);
+  
+
+  if(digitalRead(A1)==0)
+  {
+    while(digitalRead(A1)==0); //wait for release
+    LEDState=!LEDState;
+    writeLEDonoff(LEDState);
+  }
+ 
+  delay(40);
+}
+
+void writeServo(byte pos)
+{
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write('S');
+  Serial.write(pos);
+
+}
+
+void writeLED(byte Brightness)
+{
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write('B');
+  Serial.write(Brightness);
+}
+
+void writeLEDonoff(byte onoff)
+{
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write('L');
+  Serial.write(onoff);
+}
+  
+
+
